b623e7b298150dec6705dc092984995c10bf0fed
miduki.I <miduki515@stu.kanazawa-u.ac.jp>
2024-12-24 16:47:19 +0900

    微修正

--- simulation/sim3.py
+++ simulation/sim3.py
@@ -4,27 +4,27 @@ import netsquid as ns
 import pydynaa as pd
 
 from netsquid.components import ClassicalChannel, QuantumChannel
+from netsquid.components.instructions import INSTR_MEASURE, INSTR_CNOT, IGate
+from netsquid.components.component import Message, Port
+from netsquid.components.qsource import QSource, SourceStatus
+from netsquid.components.qprocessor import QuantumProcessor
+from netsquid.components.qprogram import QuantumProgram
+from netsquid.components.models.delaymodels import FixedDelayModel, FibreDelayModel
+from netsquid.components.models import DepolarNoiseModel
 from netsquid.util.simtools import sim_time
 from netsquid.util.datacollector import DataCollector
 from netsquid.qubits.ketutil import outerprod
 from netsquid.qubits.ketstates import s0, s1
 from netsquid.qubits import operators as ops
 from netsquid.qubits import qubitapi as qapi
+from netsquid.qubits import ketstates as ks
+from netsquid.qubits.state_sampler import StateSampler
 from netsquid.protocols.nodeprotocols import NodeProtocol, LocalProtocol
 from netsquid.protocols.protocol import Signals
 from netsquid.nodes.node import Node
 from netsquid.nodes.network import Network
-from netsquid.examples.entanglenodes import EntangleNodes
-from netsquid.components.instructions import INSTR_MEASURE, INSTR_CNOT, IGate
-from netsquid.components.component import Message, Port
-from netsquid.components.qsource import QSource, SourceStatus
-from netsquid.components.qprocessor import QuantumProcessor
-from netsquid.components.qprogram import QuantumProgram
-from netsquid.qubits import ketstates as ks
-from netsquid.qubits.state_sampler import StateSampler
-from netsquid.components.models.delaymodels import FixedDelayModel, FibreDelayModel
-from netsquid.components.models import DepolarNoiseModel
 from netsquid.nodes.connections import DirectConnection
+from netsquid.examples.entanglenodes import EntangleNodes
 from pydynaa import EventExpression
 
 __all__ = [
@@ -331,7 +331,7 @@ if __name__ == "__main__":
     network = example_network_setup()
     filt_example, dc = example_sim_setup(network.get_node("node_A"),
                                          network.get_node("node_B"),
-                                         num_runs=10)
+                                         num_runs=1000)
     filt_example.start()
     ns.sim_run()
     print("Average fidelity of generated entanglement with filtering: {}".format(
