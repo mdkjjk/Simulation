780bf004607e6bef11a539eadaa056e04bda2682
miduki.I <miduki515@stu.kanazawa-u.ac.jp>
2024-12-27 17:20:34 +0900

    テレポーテーション操作の追加

--- simulation/sim1.py
+++ simulation/sim1.py
@@ -38,8 +38,12 @@ class Example(LocalProtocol):
                                            num_pairs=1, name="entangle_A"))
         self.add_subprotocol(EntangleNodes(node=node_b, role="receiver", input_mem_pos=0,
                                            num_pairs=1, name="entangle_B"))
+        self.add_subprotocol(BellMeasurement(node=node_a, port=node_a.ports["cout_bob"], qubit_protocol=InitState(node_a), name="teleport_A"))
+        self.add_subprotocol(Correction(node=node_b, name="teleport_B"))
         # Set start expressions
         self.subprotocols["entangle_A"].start_expression = self.subprotocols["entangle_A"].await_signal(self, Signals.WAITING)
+        self.subprotocols["teleport_A"].start_expression = self.subprotocols["teleport_A"].await_signal(self.subprotocols["entangle_A"], Signals.SUCCESS)
+        self.subprotocols["teleport_B"].start_expression = self.subprotocols["teleport_B"].await_signal(self.subprotocols["entangle_B"], Signals.SUCCESS)
 
     def run(self):
         self.start_subprotocols()
@@ -47,19 +51,109 @@ class Example(LocalProtocol):
             start_time = sim_time()
             self.subprotocols["entangle_A"].entangled_pairs = 0
             self.send_signal(Signals.WAITING)
-            yield (self.await_signal(self.subprotocols["entangle_A"], Signals.SUCCESS) &
-                   self.await_signal(self.subprotocols["entangle_B"], Signals.SUCCESS))
-            signal_A = self.subprotocols["entangle_A"].get_signal_result(Signals.SUCCESS,
-                                                                       self)
-            signal_B = self.subprotocols["entangle_B"].get_signal_result(Signals.SUCCESS,
-                                                                       self)
+            yield (self.await_signal(self.subprotocols["teleport_A"], Signals.SUCCESS) &
+                   self.await_signal(self.subprotocols["teleport_B"], Signals.SUCCESS))
+            resurl_A = self.subprotocols["teleport_A"].get_signal_result(Signals.SUCCESS, self)
+            signal_B = self.subprotocols["teleport_B"].get_signal_result(Signals.SUCCESS, self)
             result = {
-                "pos_A": signal_A,
+                "pos_A0": resurl_A["pos_A0"],
+                "pos_A1": resurl_A["pos_A1"],
                 "pos_B": signal_B,
                 "time": sim_time() - start_time,
-                "pairs": self.subprotocols["entangle_A"].entangled_pairs,
             }
             self.send_signal(Signals.SUCCESS, result)
+
+
+class InitState(NodeProtocol):
+    def __init__(self, node, name=None, start_expression=None):
+        super().__init__(node, name)
+        self.start_expression = start_expression
+
+    def run(self):
+        while True:
+            if self.start_expression is not None:
+                yield self.start_expression
+                qubit, = qapi.create_qubits(1)
+                mem_pos = self.node.qmemory.unused_positions[0]
+                self.node.qmemory.put(qubit, mem_pos)
+                self.node.qmemory.operate(ns.H, mem_pos)
+                self.node.qmemory.operate(ns.S, mem_pos)
+                self.send_signal(signal_label=Signals.SUCCESS, result=mem_pos)
+
+
+class BellMeasurement(NodeProtocol):
+    def __init__(self, node, port, qubit_protocol, start_expression=None, name=None):
+        super().__init__(node, name)
+        self.port = port
+        self.qubit_protocol = qubit_protocol
+        self.start_expression = start_expression
+        self._qmem_pos0 = None
+        self._qmem_pos1 = None
+        self.add_subprotocol(self.qubit_protocol, "initstate")
+        self.subprotocols["initstate"].start_expression = self.subprotocols["initstate"].await_signal(self, Signals.WAITING)
+
+    def start(self):
+        super().start()
+        self.start_subprotocols()
+        if self.start_expression is not None and not isinstance(self.start_expression, EventExpression):
+            raise TypeError("Start expression should be a {}, not a {}".format(EventExpression, type(start_expression)))
+
+    def run(self):
+        qubit_initialised = False
+        entanglement_ready = False
+        yield self.start_expression
+        entanglement_ready = True
+        source_protocol = self.start_expression.atomic_source
+        ready_signal = source_protocol.get_signal_by_event(event=self.start_expression.triggered_events[0], receiver=self)
+        self._qmem_pos1 = ready_signal.result
+        self.send_signal(Signals.WAITING)
+        evexpr_signal = self.await_signal(self.subprotocols["initstate"], Signals.SUCCESS)
+        yield evexpr_signal
+        qubit_initialised = True
+        self._qmem_pos0 = self.subprotocols["initstate"].get_signal_result(Signals.SUCCESS)
+        if qubit_initialised and entanglement_ready:
+            self.node.qmemory.operate(ns.CNOT, [self._qmem_pos0, self._qmem_pos1])
+            self.node.qmemory.operate(ns.H, self._qmem_pos0)
+            m, _ = self.node.qmemory.measure([self._qmem_pos0, self._qmem_pos1])
+            # Send measurement results to Bob:
+            self.port.tx_output(m)
+            result = {"pos_A0": self._qmem_pos0,
+                      "pos_A1": self._qmem_pos1,}
+            self.send_signal(Signals.SUCCESS, result)
+
+
+class Correction(NodeProtocol):
+    def __init__(self, node, start_expression=None, name=None):
+        super().__init__(node, name)
+        self.start_expression = start_expression
+        self._qmem_pos = None
+
+    def run(self):
+        port_alice = self.node.ports["cin_alice"]
+        port_charlie = self.node.ports["qin_charlie"]
+        entanglement_ready = False
+        meas_results = None
+        while True:
+            print(f"{self.name}: Start")
+            evexpr_port_a = self.await_port_input(port_alice)
+            evexpr_port_c = self.await_port_input(port_charlie)
+            expression = yield evexpr_port_a | evexpr_port_c
+            if expression.first_term.value:
+                meas_results = port_alice.rx_input().items
+                print("result received")
+            else:
+                entanglement_ready = True
+                print("entanglement received")
+            if meas_results is not None and entanglement_ready:
+                # Do corrections (blocking)
+                if meas_results[0] == 1:
+                    self.node.qmemory.execute_instruction(instr.INSTR_Z)
+                if meas_results[1] == 0:
+                    self.node.qmemory.execute_instruction(instr.INSTR_X)
+                self.send_signal(Signals.SUCCESS, 0)
+                print("Teleport success")
+                entanglement_ready = False
+                meas_results = None
 
 
 def example_network_setup(source_delay=1e5, source_fidelity_sq=1.0, depolar_rate=0,
@@ -86,14 +180,14 @@ def example_network_setup(source_delay=1e5, source_fidelity_sq=1.0, depolar_rate
                          models={"delay_model": FibreDelayModel(c=200e3)}),
         ClassicalChannel("CChannel_B->A", length=node_distance,
                          models={"delay_model": FibreDelayModel(c=200e3)}))
-    network.add_connection(node_a, node_b, connection=conn_cchannel)
+    network.add_connection(node_a, node_b, connection=conn_cchannel, port_name_node1="cout_bob", port_name_node2="cin_alice")
     # node_A.connect_to(node_B, conn_cchannel)
     qchannel = QuantumChannel("QChannel_A->B", length=node_distance,
                               models={"quantum_noise_model": DepolarNoiseModel(4000),
                                       "delay_model": FibreDelayModel(c=200e3)},
                               depolar_rate=0)
     port_name_a, port_name_b = network.add_connection(
-        node_a, node_b, channel_to=qchannel, label="quantum")
+        node_a, node_b, channel_to=qchannel, label="quantum", port_name_node1="qin_charlie", port_name_node2="qin_charlie")
     # Link Alice ports:
     node_a.subcomponents["QSource_A"].ports["qout1"].forward_output(
         node_a.ports[port_name_a])
@@ -112,10 +206,11 @@ def example_sim_setup(node_a, node_b, num_runs):
         protocol = evexpr.triggered_events[-1].source
         result = protocol.get_signal_result(Signals.SUCCESS)
         # Record fidelity
-        q_A, = node_a.qmemory.pop(positions=[result["pos_A"]])
+        q_A0, = node_a.qmemory.pop(positions=[result["pos_A0"]])
+        q_A1, = node_a.qmemory.pop(positions=[result["pos_A1"]])
         q_B, = node_b.qmemory.pop(positions=[result["pos_B"]])
-        f2 = qapi.fidelity([q_A, q_B], ks.b01, squared=True)
-        return {"F2": f2, "pairs": result["pairs"], "time": result["time"]}
+        f2 = qapi.fidelity(q_B, ks.y0, squared=True)
+        return {"F2": f2, "time": result["time"]}
 
     dc = DataCollector(record_run, include_time_stamp=False,
                        include_entity_name=False)
@@ -128,7 +223,7 @@ if __name__ == "__main__":
     network = example_network_setup()
     example, dc = example_sim_setup(network.get_node("node_A"),
                                          network.get_node("node_B"),
-                                         num_runs=1000)
+                                         num_runs=10)
     example.start()
     ns.sim_run()
     print("Average fidelity of generated entanglement: {}".format(dc.dataframe["F2"].mean()))
