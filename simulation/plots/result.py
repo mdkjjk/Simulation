4759fe3c52d5ab205e10dabb052d393ab297313a
miduki.I <miduki515@stu.kanazawa-u.ac.jp>
2025-01-14 15:18:36 +0900

    結果まとめ

--- simulation/plots/result.py
+++ simulation/plots/result.py
@@ -3,27 +3,27 @@ import matplotlib, os
 import matplotlib.pyplot as plt
 
 matplotlib.use('Agg')
-input_csv1 = pd.read_csv("./Entanglement fidelity _3.csv")
-input_csv2 = pd.read_csv("./Entanglement fidelity with filtering_1.csv")
-input_csv3 = pd.read_csv("./Entanglement fidelity with distil_1.csv")
-input_csv4 = pd.read_csv("./Entanglement fidelity with distil & filtering_2.csv")
+input_csv1 = pd.read_csv("./Original_Entanglement fidelity_2.csv")
+input_csv2 = pd.read_csv("./Filtering_Entanglement fidelity_2.csv")
+input_csv3 = pd.read_csv("./Distil_Entanglement fidelity_2.csv")
+input_csv4 = pd.read_csv("./Distil&Filtering_Entanglement fidelity_2.csv")
 
-input_csv11 = pd.read_csv("./Teleportation fidelity_1.csv")
-input_csv22 = pd.read_csv("./Teleportation fidelity with filtering_1.csv")
-input_csv33 = pd.read_csv("./Teleportation fidelity with distil_1.csv")
-input_csv44 = pd.read_csv("./Teleportation fidelity with distil & filtering_2.csv")
+input_csv11 = pd.read_csv("./Original_Teleportation fidelity_2.csv")
+input_csv22 = pd.read_csv("./Filtering_Teleportation fidelity_2.csv")
+input_csv33 = pd.read_csv("./Distil_Teleportation fidelity_2.csv")
+input_csv44 = pd.read_csv("./Distil&Filtering_Teleportation fidelity_2.csv")
 
-data1 = input_csv1.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
-data2 = input_csv2.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
-data3 = input_csv3.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
-data4 = input_csv4.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data1 = input_csv1.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data2 = input_csv2.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data3 = input_csv3.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data4 = input_csv4.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
 
-data11 = input_csv11.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
-data22 = input_csv22.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
-data33 = input_csv33.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
-data44 = input_csv44.groupby("depolar_rate")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data11 = input_csv11.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data22 = input_csv22.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data33 = input_csv33.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
+data44 = input_csv44.groupby("node_distance")['F2'].agg(fidelity='mean', sem='sem').reset_index()
 
-xcolumn = data1['depolar_rate']
+xcolumn = data1['node_distance']
 
 save_dir = "."
 existing_files1 = len([f for f in os.listdir(save_dir) if f.startswith("Result_entanglement")])
@@ -37,7 +37,7 @@ plt.errorbar(xcolumn, data2['fidelity'], yerr=data2['sem'], marker="o", label='F
 plt.errorbar(xcolumn, data3['fidelity'], yerr=data3['sem'], marker="o", label='Distil')
 plt.errorbar(xcolumn, data4['fidelity'], yerr=data4['sem'], marker="o", label='Distil&Filtering')
 
-plt.xlabel('depolar_rate')
+plt.xlabel('node_distance')
 plt.ylabel('Fidelity')
 plt.title('Fidelity of entanglement')
 plt.legend()
@@ -52,7 +52,7 @@ plt.errorbar(xcolumn, data22['fidelity'], yerr=data2['sem'], marker="o", label='
 plt.errorbar(xcolumn, data33['fidelity'], yerr=data3['sem'], marker="o", label='Distil')
 plt.errorbar(xcolumn, data44['fidelity'], yerr=data4['sem'], marker="o", label='Distil&Filtering')
 
-plt.xlabel('depolar_rate')
+plt.xlabel('node_distance')
 plt.ylabel('Fidelity')
 plt.title('Fidelity of teleportation')
 plt.legend()
